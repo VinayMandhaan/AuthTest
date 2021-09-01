bed950bd37c92cfc11201adfffb8b1969d4ae04c
VinayMandhaan <40740604+VinayMandhaan@users.noreply.github.com>
2021-09-01 13:33:50 +0500

    ant updated

--- src/App.js
+++ src/App.js
@@ -42,62 +42,68 @@ const App = () => {
     })
   }
   return (
+
     <div>
-      <Form
-        name="basic"
-        labelCol={{
-          span: 8,
-        }}
-        wrapperCol={{
-          span: 16,
-        }}
-        initialValues={{
-          remember: true,
-        }}
-      // onFinish={onFinish}
-      // onFinishFailed={onFinishFailed}
-      >
-        <Form.Item
-
-          label="Username"
-          name="username"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your username!',
-            },
-          ]}
-        >
-          <Input onChange={e => setEmail(e.target.value)} />
-        </Form.Item>
-
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your password!',
-            },
-          ]}
-        >
-          <Input.Password onChange={e => setPassword(e.target.value)} />
-        </Form.Item>
-
-        <Form.Item
-          wrapperCol={{
-            offset: 8,
-            span: 16,
-          }}
-        >
-          <Button onClick={() => handleApiCall()} type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-
-
-
-      </Form>
+      {
+            !loggedIn && (
+              <Form
+              name="basic"
+              labelCol={{
+                span: 8,
+              }}
+              wrapperCol={{
+                span: 16,
+              }}
+              initialValues={{
+                remember: true,
+              }}
+            // onFinish={onFinish}
+            // onFinishFailed={onFinishFailed}
+            >
+              <Form.Item
+      
+                label="Username"
+                name="username"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your username!',
+                  },
+                ]}
+              >
+                <Input onChange={e => setEmail(e.target.value)} />
+              </Form.Item>
+      
+              <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your password!',
+                  },
+                ]}
+              >
+                <Input.Password onChange={e => setPassword(e.target.value)} />
+              </Form.Item>
+      
+              <Form.Item
+                wrapperCol={{
+                  offset: 8,
+                  span: 16,
+                }}
+              >
+                <Button onClick={() => handleApiCall()} type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Form.Item>
+      
+      
+      
+            </Form>
+              )
+      }
+      
 
       {
         loggedIn ? (
